bd28901eeedc6345403a05b99d986091bda8e2d9
adii <77727680+frozen-beak@users.noreply.github.com>
2024-12-26 19:53:15 +0530

    added usage of table

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,24 @@
-fn main() {}
+use turbo_cache::table::HashTable;
+
+fn main() {
+    let mut table = HashTable::new();
+
+    for i in 0..5 {
+        let key = format!("{i}");
+
+        table.set(&key, &key);
+    }
+
+    for i in 0..7 {
+        let key = format!("{i}");
+
+        match table.get(&key) {
+            Some(val) => {
+                println!("Value: {val}")
+            }
+            None => {
+                eprintln!("Value: NaN")
+            }
+        }
+    }
+}
