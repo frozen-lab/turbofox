d298afb771d67f182670aba875c18192487204e3
adii <77727680+frozen-beak@users.noreply.github.com>
2024-12-26 20:14:02 +0530

    benchmarks for table

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,49 @@
+use std::time::Instant;
 use turbo_cache::table::HashTable;
 
 fn main() {
     let mut table = HashTable::new();
 
-    for i in 0..5 {
-        let key = format!("{i}");
+    // benchmark set operation
+    let start = Instant::now();
+
+    for i in 0..100000 {
+        let key = i.to_string();
 
         table.set(&key, &key);
     }
 
-    for i in 0..7 {
-        let key = format!("{i}");
+    let duration = start.elapsed();
+
+    println!("Time taken for inserting 100K items: {:?}", duration);
+
+    // benchmark get operation
+    let start = Instant::now();
+
+    let mut not_found = 0;
+
+    for i in 0..100000 {
+        let key = i.to_string();
 
         match table.get(&key) {
             Some(val) => {
-                println!("Value: {val}")
+                if &val != &key {
+                    eprintln!("[ERR] Value mismatched, {key}:{val}");
+
+                    not_found += 1;
+                }
             }
             None => {
-                eprintln!("Value: NaN")
+                not_found += 1;
             }
         }
     }
+
+    let duration = start.elapsed();
+
+    if not_found > 0 {
+        eprintln!("[ERR] 404:{not_found}");
+    }
+
+    println!("Time taken for retrieval of 100K items: {:?}", duration);
 }
