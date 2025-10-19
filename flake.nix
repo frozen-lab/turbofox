a7a07479627ab8079e4c2aa5acc099778e2d918d
Adii <77727680+AdityaMotale@users.noreply.github.com>
2025-10-19 21:13:51 +0530

    updated flake
    - Flake is more modular now w/ outside deps

--- flake.nix
+++ flake.nix
@@ -1,43 +1,24 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
+    rust.url = "github:adityamotale/dotfiles?dir=rust";
   };
 
-  outputs = {  nixpkgs, flake-utils, ... }:
+  outputs = { nixpkgs, flake-utils, rust, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
       in {
-        devShells = {
-          default = pkgs.mkShell {
-            name = "dev";
-            buildInputs = with pkgs; [
-              # c
-              gcc
-              gdb
-              perf
-            
-              # rust
-              rustc
-              cargo
-              rustfmt
-              clippy
-              rust-analyzer
-
-              # python
-              python314
-              ruff
-              uv
-              pyright
-            ];
-
-            shellHook = ''
-              # export RUST_BACKTRACE="full"
-              echo " : $(rustc --version)"
-            '';
-          };
+        devShells.default = pkgs.mkShell {
+          name = "dev";
+          inputsFrom = [ rust.devShells.${system}.default ];
+          buildInputs = with pkgs; [
+            # rust (bench and profile)
+            gdb
+            linuxPackages.perf
+            cargo-show-asm
+          ];
         };
-       }
-    );
+    });
 }
