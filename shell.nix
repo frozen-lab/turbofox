77e57c086bf79698567479bb363e0af87b84202c
Arctic Byte <77727680+adityamotale@users.noreply.github.com>
2025-11-03 12:00:38 +0530

    (nix) `flake.nix` => `shell.nix`

---
+++ shell.nix
@@ -0,0 +1,24 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+let
+  isLinux  = builtins.match ".*-linux" pkgs.system != null;
+in
+pkgs.mkShell {
+  name = "dev";
+  buildInputs = with pkgs; [
+    rustc
+    cargo
+    rustfmt
+    clippy
+    rust-analyzer
+    cargo-show-asm
+  ]
+  ++ (if isLinux then [ pkgs.gcc pkgs.linuxPackages.perf pkgs.gdb ] else []);
+
+  nativeBuildInputs = [ pkgs.pkg-config pkgs.cmake ];
+
+  shellHook = ''
+    export RUST_BACKTRACE=1
+    export CARGO_TERM_COLOR=always
+  '';
+}
