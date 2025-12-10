0f08573bbc2ccc9f9b5020af0f3341f2898b1d33
Arctic Byte <77727680+adityamotale@users.noreply.github.com>
2025-12-10 20:25:50 +0530

    (error) impl of `TurboResult` and `TurboError`

--- src/error.rs
+++ src/error.rs
@@ -1,14 +1,37 @@
+pub type TurboResult<T> = Result<T, TurboError>;
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum TurboError {
+    IO(String),
+    InvalidPath(String),
+    InvalidConfig(String),
+    PermissionDenied(String),
+}
+
+impl From<InternalError> for TurboError {
+    fn from(err: InternalError) -> Self {
+        match err {
+            InternalError::IO(e) => Self::IO(e),
+            InternalError::InvalidPath(e) => Self::InvalidPath(e),
+            InternalError::InvalidConfig(e) => Self::InvalidConfig(e),
+            InternalError::PermissionDenied(e) => Self::PermissionDenied(e),
+        }
+    }
+}
+
 pub(crate) type InternalResult<T> = Result<T, InternalError>;
 
+#[derive(Debug, PartialEq, Eq)]
 pub(crate) enum InternalError {
     IO(String),
     InvalidPath(String),
-    Misc(String),
+    InvalidConfig(String),
+    PermissionDenied(String),
 }
 
 impl From<std::io::Error> for InternalError {
     fn from(e: std::io::Error) -> Self {
-        InternalError::IO(e.to_string())
+        InternalError::IO(format!("{}", e))
     }
 }
 
@@ -17,7 +40,8 @@ impl std::fmt::Display for InternalError {
         match self {
             Self::IO(msg) => write!(f, "{msg}"),
             Self::InvalidPath(msg) => write!(f, "{msg}"),
-            Self::Misc(msg) => write!(f, "{msg}"),
+            Self::InvalidConfig(msg) => write!(f, "{msg}"),
+            Self::PermissionDenied(msg) => write!(f, "{msg}"),
         }
     }
 }
