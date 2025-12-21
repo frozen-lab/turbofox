05afb225a75bb145a2a18b9b5f4e6d97d85abf83
Arctic Byte <77727680+adityamotale@users.noreply.github.com>
2025-12-21 15:31:16 +0530

    (cfg) new and improved `TurboConfig`

--- src/cfg.rs
+++ src/cfg.rs
@@ -1,15 +1,7 @@
-use crate::{
-    error::{TurboError, TurboResult},
-    logger::Logger,
-    utils::{is_pow_of_2, prep_directory, unlikely},
-};
-use std::path::{Path, PathBuf};
-
-const DEFAULT_INIT_CAP: usize = 0x400;
-const DEFAULT_GROWTH_FACTOR: usize = 2;
-
-pub(crate) const DEFAULT_BUF_SIZE: usize = 0x40;
-pub(crate) const MAX_KEY_SIZE: usize = 0x40;
+const DEFAULT_GROWTH_FACTOR: u32 = 2;
+const DEFAULT_BUF_SIZE: TurboConfigValue = TurboConfigValue::N64;
+const DEFAULT_INIT_CAP: TurboConfigValue = TurboConfigValue::N128;
+const DEFAULT_MAX_KEY_LEN: TurboConfigValue = TurboConfigValue::N64;
 
 /// Logging verbosity levels for [TurboConfig]
 ///
@@ -53,136 +45,199 @@ pub enum TurboLogLevel {
 /// ## Default Config
 ///
 /// - `logging`: *disabled*
-/// - `init_cap`: *1024 entries*
+/// - `buf_size`: *64 bytes*
+/// - `max_key_len`: *64 bytes*
 /// - `growth_factor`: *2x*
+/// - `initial_capacity`: *128 entries*
 ///
-/// ## Directory
+/// ## Memory Overhead
 ///
-/// The directory at provided `dirpath` **is created if not already**
+/// TODO: fill in after finalizing
 ///
-/// In case it already exists, it is reused
+/// ## Disk Overhead
 ///
-/// **⚠️ WARN:** The directory *must be empty* to avoid accidental damage of pre-existing files
-#[derive(Debug, Clone)]
+/// TODO: fill in after finalizing
+///
+/// # Example
+///
+/// ```
+/// use turbofox::{TurboConfig, TurboLogLevel, TurboConfigValue};
+///
+/// let cfg = TurboConfig {
+///   logging: true,
+///   growth_factor: 4,
+///   log_level: TurboLogLevel::INFO,
+///   initial_capacity: TurboConfigValue::N1024,
+///   max_key_len: TurboConfigValue::N128,
+///   buf_size: TurboConfigValue::N128,
+/// };
+/// ```
+#[derive(Debug)]
 pub struct TurboConfig {
-    dirpath: PathBuf,
-    logger: Logger,
-    init_cap: usize,
-    growth_factor: usize,
-}
-
-// sanity check
-const _: () = assert!(std::mem::size_of::<TurboConfig>() == 0x40);
-
-impl TurboConfig {
-    /// Builds a new [TurboConfig] using provided `dirpath`
+    /// Controls _logging visibility_ for [TurboFox]
     ///
-    /// ## Directory
+    /// By default logging is disabled
     ///
-    /// The directory at provided `dirpath` **is created if not already**
-    ///
-    /// In case it already exists, it is reused
-    ///
-    /// **⚠️ WARN:** The directory *must be empty* to avoid accidental damage of pre-existing files
-    ///
-    /// ## Default Config
-    ///
-    /// - `logging`: *disabled*
-    /// - `init_cap`: *1024 entries*
-    /// - `buf_size`: *64 bytes*
-    /// - `max_key_len`: *64 bytes*
-    /// - `growth_factor`: *2x*
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// use turbofox::TurboConfig;
-    ///
-    /// let cfg = TurboConfig::new("/tmp/test_data/turbodb");
-    /// assert!(cfg.is_ok());
-    /// ```
-    pub fn new<P: AsRef<Path>>(dirpath: P) -> TurboResult<Self> {
-        let logger = Logger::default();
-        let dirpath = dirpath.as_ref().to_path_buf();
-
-        prep_directory(&dirpath, &logger)?;
-
-        Ok(Self {
-            logger,
-            dirpath,
-            growth_factor: DEFAULT_GROWTH_FACTOR,
-            init_cap: DEFAULT_INIT_CAP,
-        })
-    }
-
-    /// Configure _initial capacity_ for `TurboFox`
-    ///
-    /// This value represents the number of entries the database holds before any
-    /// growth is triggered
-    ///
-    /// ## Constraints
-    ///
-    /// - Must be **power of 2**
-    ///
-    /// ## Tips
-    ///
-    /// - For larger work loads, use higher `init_cap`, e.g. _8192_ to avoid growth and rehash cost
-    /// - For smaller work loads, use lower `init_cap`, e.g. _32_, to reduce memory usage
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// use turbofox::{TurboConfig, TurboError};
-    ///
-    /// let cfg = TurboConfig::new("/tmp/test_data/turbodb/")?.init_cap(0x400)?;
-    /// # Ok::<(), TurboError>(())
-    /// ```
-    pub fn init_cap(mut self, cap: usize) -> TurboResult<Self> {
-        if unlikely(cap == 0) {
-            return Err(TurboError::InvalidConfig("init_cap should never be zero".into()));
-        }
-
-        if !is_pow_of_2(cap as usize) {
-            return Err(TurboError::InvalidConfig("init_cap must be power of 2".into()));
-        }
-
-        self.init_cap = cap;
-        Ok(self)
-    }
-
-    /// Configure _logging visibility_ for `TurboFox`
-    ///
-    /// Default verbosity level is [`TurboLogLevel::Error`]
+    /// When enabled, default verbosity level is `TurboLogLevel::ERROR`
     ///
     /// Use [log_level](TurboConfig::log_level) to control verbosity.
     ///
     /// # Example
     ///
     /// ```
-    /// use turbofox::{TurboConfig, TurboError};
+    /// use turbofox::TurboConfig;
     ///
-    /// let cfg = TurboConfig::new("/tmp/test_data/turbodb/")?.logging(true);
-    /// # Ok::<(), TurboError>(())
+    /// let cfg = TurboConfig {
+    ///   logging: true,
+    ///   ..Default::default()
+    /// };
     /// ```
-    pub fn logging(mut self, enable: bool) -> Self {
-        self.logger.enable(enable);
-        self
-    }
+    pub logging: bool,
 
-    /// Configure _logging verbosity level_ for `TurboFox`
+    /// Controls _growth factor_ for [TurboFox]
     ///
-    /// Use `TurboLogLevel` to control verbosity.
+    /// Default growth factor is `2x`
+    ///
+    /// e.g. For `growth_facotr = 2`, when [TurboFox] reaches _~80%_ of its current cap,
+    /// database is grown to have `new_cap = current_cap * growth_factor`, i.e. 2x of the
+    /// current cap
     ///
     /// # Example
     ///
     /// ```
-    /// use turbofox::{TurboConfig, TurboError, TurboLogLevel};
+    /// use turbofox::TurboConfig;
     ///
-    /// let cfg = TurboConfig::new("/tmp/test_data/turbodb/")?.log_level(TurboLogLevel::INFO);
-    /// # Ok::<(), TurboError>(())
+    /// let cfg = TurboConfig {
+    ///   growth_factor: 4,
+    ///   ..Default::default()
+    /// };
     /// ```
-    pub fn log_level(mut self, level: TurboLogLevel) -> Self {
-        self.logger.set_level(level);
-        self
+    pub growth_factor: u32,
+
+    /// Controls _logging verbosity level_ for [TurboFox]
+    ///
+    /// Default verbosity level is `TurboLogLevel::Error`
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use turbofox::{TurboConfig, TurboLogLevel};
+    ///
+    /// let cfg = TurboConfig {
+    ///   logging: true,
+    ///   log_level: TurboLogLevel::INFO,
+    ///   ..Default::default()
+    /// };
+    /// ```
+    pub log_level: TurboLogLevel,
+
+    /// Controls _internal buffer size_ used for storing raw Key-Value bytes in [TurboFox]
+    ///
+    /// Default _buf size_ is `TurboConfigValue::N64`
+    ///
+    /// On disk storage has a buffered layout, where each buffer is of _buf_size_, e.g. key-value
+    /// buffer is of 200 bytes, and `buf_size = 64`, the raw bytes would be stored across *4 buffers*
+    ///
+    /// For smaller key-value buffers, use smaller _buf_size_, and larger for larger buffers, for
+    /// optimal performance and storage overheads
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use turbofox::{TurboConfig, TurboConfigValue};
+    ///
+    /// let cfg = TurboConfig {
+    ///   buf_size: TurboConfigValue::N128,
+    ///   ..Default::default()
+    /// };
+    /// ```
+    pub buf_size: TurboConfigValue,
+
+    /// Controls _maximum allowed key size_ for [TurboFox]
+    ///
+    /// Default _max key len_ is `TurboConfigValue::N64`
+    ///
+    /// **NOTE:** Make sure `max_key_len == buf_size` for optimal performance and resurce usage
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use turbofox::{TurboConfig, TurboConfigValue};
+    ///
+    /// let cfg = TurboConfig {
+    ///   buf_size: TurboConfigValue::N128,
+    ///   max_key_len: TurboConfigValue::N128,
+    ///   ..Default::default()
+    /// };
+    /// ```
+    pub max_key_len: TurboConfigValue,
+
+    /// Controls _initial database capacity_ for [TurboFox]
+    ///
+    /// Default _initial cap_ is `TurboConfigValue::N128`
+    ///
+    /// This value represents the number of entries the database holds before any
+    /// growth is triggered
+    ///
+    /// ## Tips
+    ///
+    /// - For larger work loads, use higher `init_cap`, e.g. 8192 to avoid the growth cost
+    /// - For smaller work loads, use lower `init_cap`, e.g. 32 to reduce memory usage
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use turbofox::{TurboConfig, TurboConfigValue};
+    ///
+    /// let cfg = TurboConfig {
+    ///   initial_capacity: TurboConfigValue::N4096,
+    ///   ..Default::default()
+    /// };
+    /// ```
+    pub initial_capacity: TurboConfigValue,
+}
+
+impl Default for TurboConfig {
+    fn default() -> Self {
+        Self {
+            logging: false,
+            buf_size: DEFAULT_BUF_SIZE,
+            log_level: TurboLogLevel::ERROR,
+            max_key_len: DEFAULT_MAX_KEY_LEN,
+            initial_capacity: DEFAULT_INIT_CAP,
+            growth_factor: DEFAULT_GROWTH_FACTOR,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum TurboConfigValue {
+    N32,
+    N64,
+    N128,
+    N256,
+    N512,
+    N1024,
+    N2048,
+    N4096,
+    N8192,
+    N16384,
+}
+
+impl TurboConfigValue {
+    #[inline]
+    pub(crate) const fn to_usize(&self) -> usize {
+        match self {
+            Self::N32 => 0x20,
+            Self::N64 => 0x40,
+            Self::N128 => 0x80,
+            Self::N256 => 0x100,
+            Self::N512 => 0x200,
+            Self::N1024 => 0x400,
+            Self::N2048 => 0x800,
+            Self::N4096 => 0x1000,
+            Self::N8192 => 0x2000,
+            Self::N16384 => 0x4000,
+        }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,5 @@ mod error;
 mod logger;
 mod utils;
 
-pub use cfg::{TurboConfig, TurboLogLevel};
+pub use cfg::{TurboConfig, TurboConfigValue, TurboLogLevel};
 pub use error::{TurboError, TurboResult};
